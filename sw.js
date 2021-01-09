453a9fd97decfa03975e55ab25e4fec7a002a443
renan augusto <renanaugustosr@gmail.com>
2021-01-09 15:27:16 -0300

    correções

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 function registerServiceWorker() {
     // registrando o service worker para navegadores com suporte
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('js/sw.js', { scope: '/' }).then(() => {
+        navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
             console.log('Service Worker registrado com sucesso.');
         }).catch(error => {
             console.log('Service Worker falhou:', error);
