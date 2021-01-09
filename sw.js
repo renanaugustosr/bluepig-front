26b05480c106d7cdd38756efa550c1fa41ca886d
renan augusto <renanaugustosr@gmail.com>
2021-01-09 16:58:19 -0300

    .

--- sw.js
+++ sw.js
@@ -1,45 +1,45 @@
-let deferredPrompt; // Allows to show the install prompt
-let setupButton;
-
-
-function installApp() {
-    // Show the prompt
-    deferredPrompt.prompt();
-    setupButton.disabled = true;
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-        .then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('PWA setup accepted');
-                // hide our user interface that shows our A2HS button
-                setupButton.style.display = 'none';
-            } else {
-                console.log('PWA setup rejected');
-            }
-            deferredPrompt = null;
-        });
-}
-function registerServiceWorker() {
-    // registrando o service worker para navegadores com suporte
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
-            console.log('Service Worker registrado com sucesso.');
-        }).catch(error => {
-            console.log('Service Worker falhou:', error);
-        });
-    }
-}
-registerServiceWorker();
-self.addEventListener('install', e => {
-    e.waitUntil(
-        // depois que o Service Worker estiver instalado,,
-        // abra um novo cache
-        caches.open('my-pwa-cache').then(cache => {
-            // adicione todas as URLs de recursos que queremos armazenar em cache
-            return cache.addAll([
-                '/',
-                '/index.html'
-            ]);
-        })
-    );
+var VERSION = '22';
+this.addEventListener('install', function(e) {
+    e.waitUntil(caches.open(VERSION).then(cache => {
+        return cache.addAll([  
+                       'http://localhost/pw/tes.css'
+    
+    
+    ]);
+    }))
 });
+this.addEventListener('fetch', function(e) {
+    var tryInCachesFirst = caches.open(VERSION).then(cache => {
+        return cache.match(e.request).then(response => {
+            if (!response) {
+                return handleNoCacheMatch(e);
+            }
+            fetchFromNetworkAndCache(e);
+            return response
+        });
+    });
+    e.respondWith(tryInCachesFirst);
+});
+this.addEventListener('activate', function(e) {
+    e.waitUntil(caches.keys().then(keys => {
+        return Promise.all(keys.map(key => {
+            if (key !== VERSION)
+                return caches.delete(key);
+        }));
+    }));
+});
+
+function fetchFromNetworkAndCache(e) {
+    if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') return;
+    return fetch(e.request).then(res => {
+        if (!res.url) return res;
+        if (new URL(res.url).origin !== location.origin) return res;
+        return caches.open(VERSION).then(cache => {
+            return res;
+        });
+    }).catch(err => console.error(e.request.url, err));
+}
+
+function handleNoCacheMatch(e) {
+    return fetchFromNetworkAndCache(e);
+}
