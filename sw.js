17467933f6b51e594f57cf108634a1a0d692d2b1
renan augusto <renanaugustosr@gmail.com>
2021-01-09 16:41:47 -0300

    x

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 let deferredPrompt; // Allows to show the install prompt
 let setupButton;
 
-window.addEventListener('beforeinstallprompt', (e) => {
+document.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
