4d80664f8ab9af13c0b3c3963e499912c674eb55
renan augusto <renanaugustosr@gmail.com>
2021-01-09 16:35:38 -0300

    .

--- sw.js
+++ sw.js
@@ -55,17 +55,3 @@ self.addEventListener('install', e => {
         })
     );
 });
-
-import gulp from 'gulp';
-import path from 'path';
-import swPrecache from 'sw-precache';
-const rootDir = '/';
-gulp.task('generate-service-worker', callback => {
-  swPrecache.write(path.join(rootDir, 'sw.js'), {
-    staticFileGlobs: [
-      // rastreia e armazena em cache todos os arquivos que correspondem a esse padrão
-      rootDir + '/**/*.{js,html,css,png,jpg,gif}',
-    ],
-    stripPrefix: rootDir
-  }, callback);
-});
