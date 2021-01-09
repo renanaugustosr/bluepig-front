87726ee8b7ba7757e18a28817867c1c7b0b37f60
renan augusto <renanaugustosr@gmail.com>
2021-01-09 15:14:19 -0300

    service work

---
+++ js/sw.js
@@ -0,0 +1,24 @@
+function registerServiceWorker() {
+    // registrando o service worker para navegadores com suporte
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
+            console.log('Service Worker registrado com sucesso.');
+        }).catch(error => {
+            console.log('Service Worker falhou:', error);
+        });
+    }
+}
+registerServiceWorker();
+self.addEventListener('install', e => {
+    e.waitUntil(
+        // depois que o Service Worker estiver instalado,,
+        // abra um novo cache
+        caches.open('my-pwa-cache').then(cache => {
+            // adicione todas as URLs de recursos que queremos armazenar em cache
+            return cache.addAll([
+                '/',
+                '/index.html'
+            ]);
+        })
+    );
+});
